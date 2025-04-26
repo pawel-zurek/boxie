ee523874487cc33f44244e247c9f6d1664069aa1
pawel-zurek <128464762+pawel-zurek@users.noreply.github.com>
2025-04-26 21:36:13 +0200

    add email and phone_no to person

--- backend/app/models/person.py
+++ backend/app/models/person.py
@@ -9,6 +9,8 @@ class Person(Base):
     name = Column(String, nullable=False)
     role = Column(String, nullable=False)
     nip = Column(Integer, nullable=True)
+    email = Column(String, nullable=True)
+    phone_no = Column(String, nullable=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     jobs = relationship("Job", back_populates="person")
--- backend/app/schemas/person.py
+++ backend/app/schemas/person.py
@@ -1,25 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class PersonBase(BaseModel):
     name: str
     role: str
     nip: Optional[int] = None
+    email: Optional[EmailStr] = None
+    phone_no: Optional[str] = None
 
 class PersonCreate(PersonBase):
     pass
-
-class PersonOut(PersonBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
 
 class PersonUpdate(BaseModel):
     name: Optional[str] = None
     role: Optional[str] = None
     nip: Optional[int] = None
+    email: Optional[EmailStr] = None
+    phone_no: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+class PersonOut(PersonBase):
+    id: int
+    owner_id: int
 
     class Config:
         orm_mode = True
