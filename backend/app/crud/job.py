73e6711beade1a6d5a634a0087bb9da8757343da
pawel-zurek <128464762+pawel-zurek@users.noreply.github.com>
2025-05-09 02:30:19 +0200

    fix stage history

--- backend/app/crud/job.py
+++ backend/app/crud/job.py
@@ -2,12 +2,17 @@ from typing import List
 from sqlalchemy.orm import Session
 from app.models.job import Job
 from app.schemas.job import JobCreate, JobUpdate
+from app.crud import status_history
 
 def create_job(db: Session, job: JobCreate, owner_id: int):
     db_job = Job(**job.dict(), owner_id=owner_id)
     db.add(db_job)
     db.commit()
     db.refresh(db_job)
+
+    if db_job.status:
+        status_history.create_status_entry(db, job_id=db_job.id, new_status=db_job.status)
+
     return db_job
 
 def get_jobs(db: Session, owner_id: int):
